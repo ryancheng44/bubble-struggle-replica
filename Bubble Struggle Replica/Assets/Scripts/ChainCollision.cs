35392a5235fe847b976588c2e457cb9132b5f679
ryancheng44 <ryancheng44@gmail.com>
2024-07-02 11:10:26 -0400

    Added Bubble Struggle Replica project

---
+++ Bubble Struggle Replica/Assets/Scripts/ChainCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ChainCollision : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Chain.IsFired = false;
+
+        if (collision.tag == "Ball")
+        {
+            collision.GetComponent<Ball>().Split();
+        }
+    }
+}
